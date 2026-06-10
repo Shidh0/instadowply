e527f37dfae6b1f7f35a08704a3de159d3c69b71
Shidh <104437041+Gamingarc16@users.noreply.github.com>
2026-06-10 05:45:15 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
-nano "Updating script."
-nano "You might need to reconfigure download count afterwards."
+echo "Updating script."
+echo "You might need to reconfigure download count afterwards."
 curl -o android_grabber.js https://raw.githubusercontent.com/Gamingarc16/instadowply/refs/heads/main/android_grabber.js
