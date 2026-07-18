8389d97377d6f2fb6ea1e80992bf46b463397c6e
Shidh <104437041+Shidh0@users.noreply.github.com>
2026-07-18 17:35:51 +0530

    Update Update.sh

--- Update.sh
+++ Update.sh
@@ -2,3 +2,6 @@
 echo "Updating script."
 echo "You might need to reconfigure download count afterwards."
 curl -o android_grabber.js https://raw.githubusercontent.com/Gamingarc16/instadowply/refs/heads/main/android_grabber.js
+curl -o package.json https://raw.githubusercontent.com/Shidh0/instadowply/refs/heads/main/package.json
+curl -o package-lock.json https://raw.githubusercontent.com/Shidh0/instadowply/refs/heads/main/package-lock.json
+curl -o configure.sh https://raw.githubusercontent.com/Shidh0/instadowply/refs/heads/main/configure.sh
